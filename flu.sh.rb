f9cd691b66ee7feb36f3489178c9914c441180c2
Eugene Ventimiglia <eventi@blinkhealth.com>
2016-06-16 17:13:47 -0400

    Adding flu.sh

---
+++ flu.sh.rb
@@ -0,0 +1,9 @@
+class Noreallyjustfuckingstopalready < Formula
+  homepage "https://github.com/eventi/noreallyjustfuckingstopalready"
+  url "https://github.com/eventi/noreallyjustfuckingstopalready/archive/0.0.1.tar.gz"
+  sha256 "80ac38922cf20559c65107fb2c84219019bda1efb28e235118024eed89500089"
+
+  def install
+      bin.install "flu.sh"
+  end
+end
