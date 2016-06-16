17ef2d7bda217de2fc73f2c928c6ac09c635c019
Eugene Ventimiglia <eventi@blinkhealth.com>
2016-06-16 17:17:10 -0400

    So apparently the name matters? Who knew?

--- flu.sh.rb
+++ flu.sh.rb
@@ -1,4 +1,4 @@
-class Noreallyjustfuckingstopalready < Formula
+class FluSh < Formula
   homepage "https://github.com/eventi/noreallyjustfuckingstopalready"
   url "https://github.com/eventi/noreallyjustfuckingstopalready/archive/0.0.1.tar.gz"
   sha256 "80ac38922cf20559c65107fb2c84219019bda1efb28e235118024eed89500089"
